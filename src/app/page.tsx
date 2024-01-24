b4c8e96f1f310a6347bf2222f5d0063242308ca6
Nélio Dias <jrneliodias@gmail.com>
2024-01-24 00:30:11 -0300

    initila commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
+
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl m-5 mx-auto">
+      <div className="flex w-full flex-col gap-3 justify-center">
+
+      <h1 className="text-center text-3xl justify-center"> TO DO LIST</h1>
+      <AddTask/>
+      <TodoList/>
+      </div>
+
+    </main>
+  );
+}
