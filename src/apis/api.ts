62f903484ad65a72bb1daafdc7938c8a312be989
Nélio Dias <jrneliodias@gmail.com>
2024-01-24 23:20:55 -0300

    create ask apis and interface

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,10 @@
-const baseUrl = 'https://localhost:3001';
+import { ITask } from "../../types/tasks";
 
+const baseUrl = "http://localhost:3001";
 
+export const getALlTodos = async ():Promise<ITask[]> => {
 
+const res = await fetch(`${baseUrl}/tasks`);
+const todos = await res.json()
+return todos
+};
