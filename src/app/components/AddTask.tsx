005fba2c65cc49b0c7124ef054e963134e1e2b69
Nélio Dias <jrneliodias@gmail.com>
2024-01-25 10:27:57 -0300

    insert modal in AddTask component

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,11 +1,19 @@
 'use client'
-import React, { useState } from 'react'
+import React, { FormEventHandler, useState } from 'react'
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import Modal from './Modal';
 
 
 export default function AddTask() {
-  const [modalOpen, setModalOpen] = useState<boolean>(false)
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskValue,setNewTaskValue] = useState<string>("");
+
+  const handleSubmitNewTodo:FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(newTaskValue)
+    setNewTaskValue("");
+
+  }
 
   return (
     <div className='flex content-center'>
@@ -17,8 +25,17 @@ export default function AddTask() {
         </span>
         <AiOutlinePlusCircle size={20} />
       </button>
-      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}> 
-        modal
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form onSubmit={handleSubmitNewTodo}>
+          <h3 className="font-bold text-lg">Add new task</h3>
+          <div className="modal-action">
+            <input type="text" placeholder="Type here" 
+            value={newTaskValue}
+            onChange={e=> setNewTaskValue(e.target.value)}
+            className="input input-bordered w-full " />
+            <button type='submit' className='btn'>Submit</button>
+          </div>
+        </form>
       </Modal>
     </div>
   )
