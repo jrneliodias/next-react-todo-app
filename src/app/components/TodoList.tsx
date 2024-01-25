0667df88b06b1d26ed3c4bb0576013af6a268628
Nélio Dias <jrneliodias@gmail.com>
2024-01-24 23:21:46 -0300

    create todo list component

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,31 +1,39 @@
 import React from 'react'
+import { ITask } from '../../../types/tasks'
 
+interface TodoListProps {
+  tasks: ITask[]
+}
 
-
-export default function TodoList() {
+export default function TodoList({tasks}:TodoListProps) {
   return (
     <div className='content-center'>
       <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-   
-        <th>Name</th>
-        <th>Job</th>
-     
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr className="hover">
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-      </tr>
-     
-    </tbody>
-  </table>
-</div>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+
+              <th>Taks</th>
+              <th>Actions</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.id} className="hover">
+                <td>{task.text}</td>
+                <td>Quality Control Specialist</td>
+              </tr>
+
+            )
+
+            )}
+            {/* row 1 */}
+
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
