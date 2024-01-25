e220b9ee6c295b3bf27fd1bde760d300cd51c41e
Nélio Dias <jrneliodias@gmail.com>
2024-01-24 21:58:16 -0300

    create apis folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="max-w-4xl m-5 mx-auto">
       <div className="flex w-full flex-col gap-3 justify-center">
-
       <h1 className="text-center text-3xl justify-center"> TO DO LIST</h1>
       <AddTask/>
       <TodoList/>
