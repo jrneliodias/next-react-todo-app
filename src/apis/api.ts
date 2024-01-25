8b9354e6aa398493565ece4bf3a48b0af35d1d44
Nélio Dias <jrneliodias@gmail.com>
2024-01-25 10:28:18 -0300

    add addTodo async function

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,9 +2,20 @@ import { ITask } from "../../types/tasks";
 
 const baseUrl = "http://localhost:3001";
 
-export const getALlTodos = async ():Promise<ITask[]> => {
+export const getALlTodos = async (): Promise<ITask[]> => {
+  const res = await fetch(`${baseUrl}/tasks`);
+  const todos = await res.json();
+  return todos;
+};
 
-const res = await fetch(`${baseUrl}/tasks`);
-const todos = await res.json()
-return todos
+export const addTodo = async (todo: ITask):Promise<ITask> => {
+  const res = await fetch(`{baseUrl}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = await res.json();
+  return newTodo;
 };
