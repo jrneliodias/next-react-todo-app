dcfd25d9ea8f7384e9d9f536cb5fd3e81bf3dcea
Nélio Dias <jrneliodias@gmail.com>
2024-01-24 23:21:34 -0300

    create async fuction to get all tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import { getALlTodos } from "@/apis/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getALlTodos();
+
+
   return (
     <main className="max-w-4xl m-5 mx-auto">
       <div className="flex w-full flex-col gap-3 justify-center">
       <h1 className="text-center text-3xl justify-center"> TO DO LIST</h1>
       <AddTask/>
-      <TodoList/>
+      <TodoList tasks = {tasks}/>
       </div>
 
     </main>
