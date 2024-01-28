4ea11c1fc519269f85f7f23888e4f4e71cf97152
Nélio Dias <jrneliodias@gmail.com>
2024-01-27 21:30:11 -0300

    create CRUD functions in API

--- src/app/apis/task/route.ts
+++ src/app/apis/task/route.ts
@@ -1,34 +1,87 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/db";
 
-export async function GET(req: NextRequest){
-    console.log(req.nextUrl.searchParams)
-    return Response.json({message:'OK'})
+export async function GET(req: NextRequest) {
+  // console.log(req.nextUrl.searchParams)
+  try {
+    const task = await prisma.task.findMany();
+
+    return Response.json({ message: "OK", task });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        error,
+      },
+      { status: 500 }
+    );
+  }
 }
 
-export async function POST(req: Request){
-    const {content,favorite,color} = await req.json()
-    console.log(content,favorite,color)
-    try {
-        const task = await prisma.task.create({
-            data:{
-                content,
-                favorite,
-                color
-            }
-        })
-        return Response.json({message:'OK',task})
-    } catch (error) {
-        return NextResponse.json(
-            {
-                message:"Error",
-                error,
-            },
-            {status:500,}
-        )
-    }
-    
-    
-  
+export async function POST(req: Request) {
+  const { content, favorite, color } = await req.json();
+
+  try {
+    const task = await prisma.task.create({
+      data: {
+        content,
+        favorite,
+        color,
+      },
+    });
+    return Response.json({ message: "OK", task });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        error,
+      },
+      { status: 500 }
+    );
+  }
 }
 
+export async function DELETE(req: NextRequest) {
+  const { id } = await req.json();
+  try {
+    const task = await prisma.task.delete({
+      where: {
+        id,
+      },
+    });
+    return NextResponse.json({ message: "OK", task });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  const { id, content, favorite, color } = await req.json();
+  try {
+    const task = await prisma.task.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        favorite,
+        color,
+      },
+    });
+    return NextResponse.json({message:"OK",task})
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
