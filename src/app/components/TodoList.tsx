157e5478916f5d60feb8b0e0d011763fef8e4bd6
Nélio Dias <jrneliodias@gmail.com>
2024-01-29 20:01:31 -0300

    favorite filter working now

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,17 +1,35 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent, useState } from 'react'
 import { ITask, ITaskColor } from '../../../types/tasks'
 import Task from './Task'
+import { useRouter } from 'next/navigation'
 
 interface TodoListProps {
   tasks: ITaskColor[]
 }
 
 export default function TodoList({ tasks }: TodoListProps) {
-// Sort the data based on the 'favorite property
-const sortedTasks = [...tasks].sort((a,b)=> (a.favorite === b.favorite? 0: a.favorite? -1:1))
+  // Sort the data based on the 'favorite property
+  const sortedTasks = [...tasks].sort((a, b) => (a.favorite === b.favorite ? 0 : a.favorite ? -1 : 1))
+  const [filterFavorite, setFilterFavorite] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleChangeFavoriteFilterValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const filterFavorite = e.target.checked
+    setFilterFavorite(filterFavorite)
+    
+}
+
+const filteredTodos = filterFavorite ? sortedTasks.filter((todo)=> todo.favorite) : sortedTasks
 
   return (
     <div className='content-center mx-5'>
+      <div className="form-control">
+        <label className=" flex gap-2 items-center cursor-pointer">
+          <input type="checkbox" onChange={handleChangeFavoriteFilterValue} defaultChecked={filterFavorite} className="checkbox checkbox-secondary" />
+          <span className="label-text "> Favoritos </span>
+        </label>
+      </div>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -26,7 +44,7 @@ const sortedTasks = [...tasks].sort((a,b)=> (a.favorite === b.favorite? 0: a.fav
             </tr>
           </thead>
           <tbody>
-            {sortedTasks.map((task) => (
+            {filteredTodos.map((task) => (
               <Task key={task.id} task={task} />
 
             ))}
