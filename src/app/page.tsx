5bccbbb11db349f2bd46bfdb8a8cac206c3a8cd1
Nélio Dias <jrneliodias@gmail.com>
2024-03-09 20:17:24 -0300

    feat: getAllTask fuction from prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 import { getAllTodos } from "@/app/apis/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import prisma from '../../lib/db'
 
+async function getAllTasks() {
+  const tasks = await prisma.task.findMany()
+  return tasks
+
+}
 
 export default async function Home() {
-  const tasks = await getAllTodos();
+  const tasks = await getAllTasks();
 
 
   return (
     <main className="max-w-4xl my-5 mx-auto">
       <div className="flex w-full flex-col gap-3 justify-center">
-      <h1 className="text-center text-3xl justify-center font-bold">TO DO LIST</h1>
-      <AddTask/>
-      <TodoList tasks = {tasks.task}/>
+        <h1 className="text-center text-3xl justify-center font-bold">TO DO LIST</h1>
+        <AddTask />
+        <TodoList tasks={tasks} />
       </div>
 
     </main>
