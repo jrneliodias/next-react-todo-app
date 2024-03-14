f68fe3d41ff12c865da826825020f2b069ac365d
Nélio Dias <jrneliodias@gmail.com>
2024-03-14 00:01:57 -0300

    fix: insert the production or developement if case to the base url

--- src/app/apis/api.ts
+++ src/app/apis/api.ts
@@ -1,8 +1,12 @@
 import { ITaskColor, TaskObject } from "../../../types/tasks";
 import prisma from "../../../lib/db";
 
-// const baseUrl = "https://todo-list-app-nelio.vercel.app";
-const baseUrl = process.env.NEXT_PUBLIC_MY_APP_LOCAL || process.env.NEXT_PUBLIC_MY_APP_URL;
+let baseUrl = process.env.NEXT_PUBLIC_MY_APP_URL;
+
+if (process.env.NODE_ENV === "development") {
+  // Code to execute when running in a development environment
+  baseUrl = process.env.NEXT_PUBLIC_MY_APP_LOCAL;
+}
 
 export const getAllTodos = async (): Promise<TaskObject> => {
   const res = await fetch(`${baseUrl}/apis/task`, { method: "GET", cache: "no-store" });
