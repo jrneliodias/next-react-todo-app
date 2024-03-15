cb2cf033ca64935fd6691c1ae3f472d405650812
Nélio Dias <jrneliodias@gmail.com>
2024-03-15 14:56:12 -0300

    core: setup jest to run tests

--- src/app/apis/task/route.ts
+++ src/app/apis/task/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/db";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const task = await prisma.task.findMany();
 
