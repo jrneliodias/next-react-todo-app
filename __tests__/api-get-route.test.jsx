e6098a411a44ccc106e95e9070f6e9f6bb928c23
Nélio Dias <jrneliodias@gmail.com>
2024-03-16 15:24:22 -0300

    test: should return 500 when get all tasks return a error

--- __tests__/api-get-route.test.jsx
+++ __tests__/api-get-route.test.jsx
@@ -2,6 +2,9 @@ import "@testing-library/jest-dom";
 import { GET } from "../src/app/apis/task/route.ts";
 
 describe("GET Route: /apis/task", () => {
+  // Mocking the Prisma task create method
+  const mockCreate = jest.spyOn(prisma.task, "findMany");
+
   it("should return a list of tasks", async () => {
     const response = await GET();
     expect(response.status).toBe(200);
@@ -18,5 +21,13 @@ describe("GET Route: /apis/task", () => {
       expect(task).toHaveProperty("color");
       expect(task).toHaveProperty("completed");
     });
+  });
+  it("should return 500 when get tasks fail", async () => {
+    mockCreate.mockRejectedValue(new Error("Failed to get all task"));
+
+    const response = await GET();
+    const tasks = await response.json();
+
+    expect(response.status).toBe(500);
   });
 });
